bb21ccba67994c7a031537f8e552eea5d29df360
IgruantyPlay <piuuttri@gmail.com>
2022-08-03 00:17:08 +0200

    Main functionality added

---
+++ grid.js
@@ -0,0 +1,64 @@
+class Grid {
+    constructor(rows, columns) {
+        this.r = rows;
+        this.c = columns;
+        this.pixelw = width / this.r;
+        this.pixelh = height / this.c;
+        this.cells = []
+        for(let i = 0; i < this.r * this.c; i++) {
+            this.cells[i] = 0;
+        }
+    }
+    full(temp, idx) {
+        return temp[idx] >= 1;
+    }
+    check(temp, x, y) {
+        if(x < 0) x = this.c - 1;
+        if(x >= this.c) x = 0;
+        if(y < 0) y = this.r - 1;
+        if(y >= this.r) y = 0;
+        return this.full(temp, x + y * this.c);
+    }
+    countNeighbors(temp, _x, _y) {
+        let m = 0;
+        for(let x = -1; x <= 1; x++)
+        {
+            for(let y = -1; y <= 1; y++) {
+                if(x === 0 && y === 0) continue;
+                m += this.check(temp, _x + x, _y + y) ? 1 : 0;
+            }
+        }
+        return m;
+    }
+    set(v, x, y) {
+        this.cells[x + y * this.c] = v;
+    }
+    get(x, y) {
+        return this.cells[x + y * this.c];
+    }
+    step() {
+        let temp = [...this.cells]
+        for(let i = 0; i < this.r; i++) {
+            for(let j = 0; j < this.c; j++) {
+                let nbs = this.countNeighbors(temp, i, j);
+                if(nbs == 2)
+                    this.set(this.get(i, j), i, j);
+                else if(nbs == 3)
+                    this.set(1, i, j);
+                else
+                    this.set(0, i, j);
+                
+            }
+        }
+    }
+    show() {
+        for(let i = 0; i < this.r; i++) {
+            for(let j = 0; j < this.c; j++) {
+                let idx = j + i * this.c;
+                stroke(0);
+                fill(this.full(this.cells, idx) ? 255 : 0);
+                rect(j * this.pixelw, i * this.pixelh, this.pixelw, this.pixelh);
+            }
+        }
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,6 +1,20 @@
+let grid;
+
 function setup() {
-    createCanvas(800, 600);
+    createCanvas(800, 800);
+    grid = new Grid(64, 64);
+    grid.set(1, 10, 10);
+    grid.set(1, 11, 11);
+    grid.set(1, 11, 12);
+    grid.set(1, 10, 13);
+    grid.set(1, 9, 12);
+    grid.set(1, 9, 11);
+    grid.set(1, 10, 12);
+    grid.set(1, 13, 13);
+    frameRate(30);
 }
 function draw() {
     background(0);
+    grid.show();
+    grid.step();
 }
